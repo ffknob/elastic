a8f116a89c96a54a7022f544d3097c3c8b74ea9c
Flávio Knob <ffknob@gmail.com>
2018-05-02 18:42:16 -0300

    ...

--- load.py
+++ load.py
@@ -8,22 +8,31 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 indexes = [
     { 'data_file': 'V_SUMULAS.json', 'index': 'jurisprudencia-sumulas', 'type': 'default'},
     { 'data_file': 'V_INFORMACOES_CT.json', 'index': 'jurisprudencia-informacoes_ct', 'type': 'default'},
-    { 'data_file': 'V_PARECERES.json', 'index': 'jurisprudencia-pareceres', 'type': 'default'},
-    { 'data_file': 'V_DECISOES.json', 'index': 'jurisprudencia-decisoes', 'type': 'default'},
+    { 'data_file': 'V_PARECERES.json', 'index': 'jurisprudencia-pareceres', 'type': 'default'}
+    #{ 'data_file': 'V_DECISOES.json', 'index': 'jurisprudencia-decisoes', 'type': 'default'}
 ]
 
-index = indexes[2]
+for index in indexes:
+    print "Loading {}...".format(index["index"])
+    total_ok = 0
+    total_error = 0
 
-with codecs.open(index['data_file'], 'rU', 'utf-8') as f:
-    data = json.loads(f.read())
+    with codecs.open(index["data_file"], 'rU', 'utf-8') as f:
+        data = json.loads(f.read())
 
-for doc in data["items"]:
-    id = doc["id_elastic_search"]
+    for doc in data["items"]:
+        id = doc["id_elastic_search"]
 
-    try:
-        es.index(index=index['index'], doc_type=index['type'], id=id, body=doc)
+        try:
+            es.index(index=index["index"], doc_type=index["type"], id=id, body=doc)
 
-        print "{}: OK".format(id)
-    except Exception as e:
-        print "{}: ERROR".format(id)
-        continue
+            #print "{} -> {}: OK".format(index["index", id)
+            total_ok += 1
+
+        except Exception as e:
+            #print "{} -> {}: ERROR".format(index["index"], id)
+            total_error += 1
+            continue
+
+    print "OK: {}".format(total_ok)
+    print "Error: {}".format(total_error)
