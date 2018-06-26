ce5ab339de2b91d09dc6aa555eff37e0f6413389
Flávio Knob <ffknob@gmail.com>
2018-06-26 12:00:00 -0300

    transformaçãod e datas

--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from datetime import date
+import re
 import json
 import codecs
 import cx_Oracle
@@ -108,13 +109,46 @@ def load_json(index):
 
     print "{} -> {} total, {} sucessos, {} erros".format(index['name'], total, sucessos, erros)
 
+def transform_json(index):
+    total = 0
+    sucessos = 0
+    erros = 0
+    with codecs.open(index['data_file'], 'rU', 'utf-8') as f:
+        data = json.loads(f.read())
+
+    nf = open(index['data_file'] + '.transformed', 'w')
+    for doc in data['items']:
+        for attr in doc.keys():
+            if(isinstance(doc[attr], basestring) and
+                re.match('^[0-9]{2}/[0-9]{2}/[0-9]{2}$', doc[attr].encode('utf-8'))):
+                day = doc[attr][0:2]
+                month = doc[attr][3:5]
+                year = doc[attr][6:8]
+
+                if(int(year) >= 0 and int(year) <= 20):
+                    year = '20' + year
+                else:
+                    year = '19' + year
+
+                d = date(int(year), int(month), int(day))
+                doc[attr] = d.strftime('%Y-%m-%d')
+
+    json.dump(data, nf)
+
+    print "{} -> {}".format(index['data_file'], index['data_file'] + '.transformed')
+
 def main():
     string_conexao = conexao['usuario'] + '/' + \
                      conexao['senha'] + '@' + \
                      conexao['servidor'] + '/' + \
                      conexao['servico']
 
-    #load_json(indexes[0])
+    #transform_json(indexes[0])
+    #transform_json(indexes[1])
+    #transform_json(indexes[2])
+    #transform_json(indexes[3])
+
+    load_json(indexes[0])
     load_json(indexes[1])
     load_json(indexes[2])
     load_json(indexes[3])
