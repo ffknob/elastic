cf2710e86382e6e5a76c9221f9350902e07de793
ffknob <ffknob@tce.rs.gov.br>
2019-07-19 15:13:44 -0300

    refactory

--- elastic-ctl.sh
+++ elastic-ctl.sh
@@ -12,21 +12,29 @@ use() {
 
 install() {
 	# Elasticsearch
-	chown 1000:1000 docker-elastic/elasticsearch/data/
-	cp -f config/elasticsearch/elasticsearch.yml docker-elastic/elasticsearch/config/elasticsearch.yml 2> /dev/null
+	echo "Installing Elasticsearch's configuration files..."
+	cp -f config/elasticsearch/elasticsearch.yml docker-elastic/elasticsearch/config/elasticsearch-${1}.yml 2> /dev/null
 
 	# Logstash
-	cp -f config/logstash/logstash.yml docker-elastic/logstash/config/logstash.yml 2> /dev/null
+	echo "Installing Logstash's configuration files..."
+	cp -f config/logstash/logstash.yml docker-elastic/logstash/config/logstash-${1}.yml 2> /dev/null
 	#cp -f config/logstash/pipeline/*.yml docker-elastic/logstash/pipeline/ 2> /dev/null
 
 	# Kibana
-	cp -f config/kibana/kibana.yml docker-elastic/kibana/config/kibana.yml 2> /dev/null
+	echo "Installing Kibana's configuration files..."
+	cp -f config/kibana/kibana.yml docker-elastic/kibana/config/kibana-${1}.yml 2> /dev/null
+
+	# APM server
+	echo "Installing APM's server configuration files..."
+	cp -f config/apm-server/apm-server.yml docker-elastic/apm-server/config/apm-server-${1}.yml 2> /dev/null
 
 	# Metricbeat
-	cp -f config/beats/metricbeat/metricbeat.yml docker-elastic/beats/metricbeat/config/metricbeat.yml 2> /dev/null
-	cp -f config/beats/metricbeat/metricbeat-host.yml docker-elastic/beats/metricbeat/config/metricbeat-host.yml 2> /dev/null
+	echo "Installing Metricbeat's configuration files..."
+	cp -f config/beats/metricbeat/metricbeat.yml docker-elastic/beats/metricbeat/config/metricbeat-${1}.yml 2> /dev/null
+	cp -f config/beats/metricbeat/metricbeat-host.yml docker-elastic/beats/metricbeat/config/metricbeat-host-${1}.yml 2> /dev/null
 
 	# Heartbeat
+	echo "Installing Heartbeat's configuration files..."
 	cp -f config/beats/heartbeat/heartbeats/*.yml docker-elastic/beats/heartbeat/heartbeats/ 2> /dev/null
 }
 
